fe4f5aaa48517b5f2d7c9c33b003bd51f2830862
libap <118845814+libap@users.noreply.github.com>
2023-04-25 02:00:27 +0200

    slide in

--- index.js
+++ index.js
@@ -36,3 +36,28 @@ const headerLogoConatiner = document.querySelector('.header__logo-container')
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
 })
+
+// Sélectionner tous les "projects__row"
+const rows = document.querySelectorAll('.projects__row');
+
+// Ajouter un gestionnaire d'événement "scroll" sur la fenêtre
+window.addEventListener('scroll', () => {
+  // Boucler sur chaque "projects__row"
+  rows.forEach(row => {
+    // Vérifier si le haut et le bas de la "projects__row" sont visibles dans la fenêtre
+    const rowTop = row.getBoundingClientRect().top;
+    const rowBottom = row.getBoundingClientRect().bottom;
+    const windowHeight = window.innerHeight;
+    if (rowTop < windowHeight && rowBottom >= 0) {
+      // Ajouter la classe "slide-in" après un délai de 500ms
+      setTimeout(() => {
+        row.classList.add('slide-in');
+        row.classList.remove('slide-in-hidden');
+      }, 250);
+    } else {
+      // Supprimer la classe "slide-in" si elle a été ajoutée précédemment
+      row.classList.remove('slide-in');
+      row.classList.add('slide-in-hidden');
+    }
+  });
+});
